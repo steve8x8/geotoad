a45a2c324692aadbfe46cc2a9a40984c37bc9d41
thomas <thomas@abbcce6f-711e-0410-a2ab-651830b4d790>
2003-04-25 20:59:59 +0000

    If you get your search key wrong, mention the mode we tried

--- geotoad/trunk/geocache/search.rb
+++ geotoad/trunk/geocache/search.rb
@@ -24,7 +24,7 @@ class SearchCache < Common
         @mode=keylookup.type
 		@key=keylookup.lookup(key)
         if (! @key)
-            puts "Bad search key: #{key}"
+            puts "Bad search key for #{@mode} type: #{key}"
             return nil
         end
         
