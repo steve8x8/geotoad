d409384c47c0fba49085e82a05a0d8b7f336efd6
tstrombe <tstrombe@abbcce6f-711e-0410-a2ab-651830b4d790>
2004-01-19 01:30:03 +0000

    3.5.0-RC2. One major packaging change: geotoad.rb is now GeoToad.rb, mostly so it looks more obvious to Windows users.
    I added a symlink to GeoToad.rb named GeoToad for Mac.command, which makes
    it so Mac users can double click on GeoToad just like Windows users can.

--- geotoad/trunk/tools/makedist.sh
+++ geotoad/trunk/tools/makedist.sh
@@ -7,10 +7,11 @@ VERSION=`cat VERSION`
 
 DIST="geotoad-$VERSION"
 LONGDIST="$DIST"
+echo "Creating /tmp/$DIST"
 rm -Rf /tmp/$DIST
 mkdir /tmp/$DIST
 cp -R TODO.txt COPYRIGHT.txt geocache CLI/interface /tmp/$DIST
-sed s/"%VERSION%"/"$VERSION"/g CLI/geotoad.rb > /tmp/$DIST/geotoad.rb
+sed s/"%VERSION%"/"$VERSION"/g CLI/geotoad.rb > /tmp/$DIST/GeoToad.rb
 sed s/"%VERSION%"/"$VERSION"/g CLI/README.txt > /tmp/$DIST/README.txt
 chmod 755 /tmp/$DIST/*.rb
 rm /tmp/$DIST/**/*~ 2>/dev/null
@@ -18,11 +19,20 @@ rm /tmp/$DIST/*/._* 2>/dev/null
 rm -Rf /tmp/$DIST/**/CVS 2>/dev/null
 rm -Rf /tmp/$DIST/**/.svn 2>/dev/null
 
+echo "Updating repository..."
 svn update
+echo "Creating Changelog"
 svn log VERSION COPYRIGHT.txt geocache CLI -v > /tmp/$DIST/ChangeLog.txt
 
+echo "Creating zipfile"
 cd /tmp
+cd $DIST
+ln -s GeoToad.rb "GeoToad for Mac.command"
+cd ..
+
 zip -r $LONGDIST.zip $DIST
+echo "Copying zipfile to webservers"
+
 scp $LONGDIST.zip /tmp/$DIST/ChangeLog.txt smtp.stromberg.org:/www/toadstool.se/htdocs/hacks/geotoad/files/
 scp $LONGDIST.zip /tmp/$DIST/ChangeLog.txt toadstool.se:/www/toadstool.se/htdocs/hacks/geotoad/files/
 echo "http://home.toadstool.se/hacks/geotoad/files/$LONGDIST.zip"
