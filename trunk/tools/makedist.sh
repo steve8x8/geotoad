a60191f4c5dc059750b12a9fa87d6eabbb0271d9
helixblue <helixblue@abbcce6f-711e-0410-a2ab-651830b4d790>
2008-09-02 12:16:07 +0000

    Update our build process -- still requires manual intervention for Mac and Windows builds

--- trunk/tools/makedist.sh
+++ trunk/tools/makedist.sh
@@ -1,70 +1,66 @@
 #!/bin/sh
 # Builds a new release of geotoad
 # $Id: makedist.se,v 1.3 2002/04/23 04:05:41 helix Exp $
-
 cd ..
 SRC=`pwd`
 VERSION=`cat VERSION`
-DIST="geotoad-$VERSION"
-DEST=/tmp/$DIST
-LONGDIST="$DIST"
-
-echo "Creating $DEST"
-rm -Rf $DEST*
-mkdir $DEST
-cp -R CLI/._* CLI/.DS* FAQ.txt COPYRIGHT.txt lib tools data CLI/interface contrib $DEST
-ditto CLI/*Mac* $DEST
-sed s/"%VERSION%"/"$VERSION"/g CLI/geotoad.rb > $DEST/geotoad.rb
-sed s/"%VERSION%"/"$VERSION"/g CLI/README.txt > $DEST/README.txt
-sed s/"%VERSION%"/"$VERSION"/g FAQ.txt > $DEST/FAQ.txt
-chmod 755 $DEST/*.rb
-rm $DEST/**/*~ 2>/dev/null
-find $DEST -name .svn -exec rm -Rf {} \;
+DISTNAME="geotoad-$VERSION"
+DEST=$HOME/Desktop/GeoToad
+DIST_SRC=$DEST/$DISTNAME
+DIST_MAC=$DEST/${DISTNAME}_for_Mac
+DIST_WIN=$DEST/${DISTNAME}_for_Windows
 
 echo "Updating repository..."
 svn update
-echo "Creating Changelog"
-svn log > $DEST/ChangeLog.txt
 
-# unnecessary tools
-rm $DEST/tools/makedist.sh
-rm $DEST/data/*.txt
+echo "Erasing old distributions."
+rm -Rf $DEST
+
+echo "Creating $DIST_SRC"
+mkdir -p $DIST_SRC
+rsync -a --exclude "*~" --exclude ".svn/" . $DIST_SRC
+sed s/"%VERSION%"/"$VERSION"/g geotoad.rb > $DIST_SRC/geotoad.rb
+sed s/"%VERSION%"/"$VERSION"/g README.txt > $DIST_SRC/README.txt
+sed s/"%VERSION%"/"$VERSION"/g FAQ.txt > $DIST_SRC/FAQ.txt
+chmod 755 $DIST_SRC/*.rb
+svn log > $DIST_SRC/ChangeLog.txt
+rm $DIST_SRC/VERSION $DIST_SRC/tools/tar2rubyscript.rb $DIST_SRC/tools/countryrip.rb
+
+# Make a duplicate of it for Macs before we nuke the .command file
+cp -Rp $DIST_SRC $DIST_MAC
+rm $DIST_SRC/*.command
+ln -s geotoad.rb geotoad
+zip -r ${DIST_SRC}.zip $DIST_SRC
 
 # Mac OS X
-cd /tmp
-echo "Creating zipfile (Mac OS X): zip -r ${LONGDIST}_for_MacOS.zip $DIST"
-zip -r ${LONGDIST}_for_MacOS.zip $DIST
-
-# Generic
-echo "Creating zipfile (Generic): zip -r $LONGDIST.zip $DIST"
-rm $DEST/*.command 
-rm $DEST/.* 2>/dev/null
-zip -r $LONGDIST.zip $DIST
+echo "Creating $DIST_MAC"
+rm $DIST_MAC/geotoad
+echo "Using Finder, rename the .command in $DIST_MAC and apply icon from data/bufos.icns"
+read 
+hdiutil create -srcfolder $DIST_MAC ${DIST_MAC}.dmg
 
 # Windows
-echo "Creating zipfile (Windows): zip -r ../${LONGDIST}_for_Windows.zip *"
-cd $DEST
-#rm -Rf *.rb lib interface
+echo "Creating $DIST_WIN"
+cp -Rp $DIST_SRC $DIST_WIN
+rm $DIST_WIN/geotoad
+cd $DIST_WIN
 mkdir compile
-mv *.rb lib interface data compile
+mv *.rb lib interface compile
 mv compile/geotoad.rb compile/init.rb
-tar2rubyscript compile
-echo "Under Windows, run: rubyscript2exe compile.rb"
-
-# convert to Windows newlines
 flip -d *.txt
-
-# remove geotoad.rb mentions, since it's an executable.
-perl -pi -e  's/([\s])geotoad\.rb/$1geotoad/g' README.txt
-
-cp $SRC/CLI/*.exe .
-zip -r ../${LONGDIST}_for_Windows.zip *
-
-echo "Copying to desktop"
-cp /tmp/${LONGDIST}*.* ~/Desktop
-
-#echo "Copying zipfile to webservers"
-#scp $LONGDIST.zip $DEST/ChangeLog.txt smtp.stromberg.org:/www/toadstool.se/htdocs/hacks/geotoad/files/
-#scp $LONGDIST.zip $DEST/ChangeLog.txt toadstool.se:/www/toadstool.se/htdocs/hacks/geotoad/files/
-#echo "http://home.toadstool.se/hacks/geotoad/files/$LONGDIST.zip"
-
+perl -pi -e 's/([\s])geotoad\.rb/$1geotoad/g' README.txt
+echo "Running tar2rubyscript.rb compile"
+ruby $SRC/tools/tar2rubyscript.rb compile
+if [ -f "compile.rb" ]; then
+  echo "Under vmware, run: ruby rubyscript2exe.rb compile.rb"
+  read ENTER
+  if [ -f "compile.exe" ]; then
+    mv compile.exe geotoad.exe
+    rm -Rf $DIST_WIN/compile
+    zip -r $DIST_WIN.zip $DIST_WIN
+  else
+    echo "compile.exe not found, FAIL."
+  fi
+else
+  echo "Failed to run tar2rubyscript.rb, get it from http://www.erikveen.dds.nl/tar2rubyscript/index.html"
+fi
