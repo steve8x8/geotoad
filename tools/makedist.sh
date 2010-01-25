b522798a7b493efbbefd9de06be59d32352bcb2b
helixblue <helixblue@abbcce6f-711e-0410-a2ab-651830b4d790>
2010-01-25 15:13:56 +0000

    Improve makedist.sh, update instructions for windows

--- tools/makedist.sh
+++ tools/makedist.sh
@@ -2,17 +2,19 @@
 # Builds a new release of geotoad
 # $Id: makedist.se,v 1.3 2002/04/23 04:05:41 helix Exp $
 
-src_dir="/tmp/namebench-$$"
-svn checkout http://geotoad.googlecode.com/svn/trunk/ $src_dir
-cd $src_dir
-
+# this must be run from the geotoad directory.
 if [ ! -f VERSION ];  then
   echo "VERSION not found"
   exit 2
 fi
+base_dir=`pwd`
+src_dir="/tmp/namebench-$$"
+svn checkout http://geotoad.googlecode.com/svn/trunk/ $src_dir
+cd $src_dir
+
 VERSION=`cat VERSION`
 DISTNAME="geotoad-$VERSION"
-DEST=$HOME/Desktop/GeoToad
+DEST="${base_dir}/dist"
 GENERIC_DIR=$DEST/$DISTNAME
 GENERIC_PKG="${GENERIC_DIR}.zip"
 
@@ -60,28 +62,29 @@ else
 fi
 
 # Windows
-if [ -r "/usr/local/bin/rubyscript2ex.rb" ]; then
+if [ ! -x "/usr/local/bin/flip" -o ! -x "/usr/bin/flip" ]; then
   echo "Creating $WIN_DIR"
   cp -Rp "$GENERIC_DIR" "$WIN_DIR"
-  rm "$WIN_DIR/geotoad"
   cd "$WIN_DIR"
   mkdir compile
   mv *.rb lib interface data compile
   mv compile/geotoad.rb compile/init.rb
-  flip -d *.txt
+  flip -m *.txt
   perl -pi -e 's/([\s])geotoad\.rb/$1geotoad/g' README.txt
 
   echo "In Windows, run: ruby rubyscript2exe.rb compile/init.rb"
   read ENTER
-  if [ -f "compile.exe" ]; then
-    mv compile.exe geotoad.exe
+  cd $WIN_DIR
+  ls
+  if [ -f "init.exe" ]; then
+    mv init.exe geotoad.exe
     mv compile/data .
     rm -Rf "$WIN_DIR/compile"
     zip -r "$WIN_PKG" *
   else
-    echo "compile.exe not found"
+    echo "init.exe not found"
   fi
 else
-  echo "Skipping Windows Release"
+  echo "Skipping Windows Release (no flip found)"
 fi
 
