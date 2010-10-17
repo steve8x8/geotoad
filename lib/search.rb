26a750ef0580a50bda484ce6747ef7b9fe11131e
helixblue <helixblue@abbcce6f-711e-0410-a2ab-651830b4d790>
2010-10-17 14:34:39 +0000

    Add ctime parsing patch by Steve8x8. Fixes issue 133

--- lib/search.rb
+++ lib/search.rb
@@ -10,9 +10,9 @@ BASE_URL = 'http://www.geocaching.com/seek/nearest.aspx'
 class SearchCache
   include Common
   include Messages
-  
+
   attr_accessor :distance
-    
+
   def initialize
     @distance = 15
     @ttl = 72000
@@ -22,7 +22,7 @@ class SearchCache
   def setType(mode, key)
     @query_type = nil
     @query_arg = key
-    supports_distance = false    
+    supports_distance = false
     case mode
     when 'location'
       @query_type = 'location'
@@ -44,7 +44,7 @@ class SearchCache
       supports_distance = true
       lat_dir, lat_h, lat_ms, long_dir, long_h, long_ms, lat_ns, long_ew = parseCoordinates(key)
       @search_url = BASE_URL + '?lat_ns=' + lat_ns.to_s + '&lat_h=' + lat_h + '&lat_mmss=' + (lat_ms==''?'0':lat_ms) + '&long_ew=' + long_ew.to_s + '&long_h=' + long_h + '&long_mmss=' + (long_ms==''?'0':long_ms)
-    
+
     when 'user'
       @query_type = 'ul'
       @ttl = 43200
@@ -55,17 +55,17 @@ class SearchCache
     when 'wid'
       @query_type = 'wid'
       @search_url = "http://www.geocaching.com/seek/cache_details.aspx?wp=#{key.upcase}"
-    end     
+    end
 
     if not @query_type
       displayWarning "Invalid query type: #{mode}. Valid types include: location, coord, keyword, wid"
       return nil
-    end      
+    end
 
     if not @search_url
         @search_url = BASE_URL + '?' + @query_type + '=' + CGI.escape(@query_arg.to_s)
     end
-    
+
     if supports_distance and @distance
         @search_url = @search_url + '&dist=' + @distance.to_s
     end
@@ -73,36 +73,36 @@ class SearchCache
     displayInfo @search_url
     return @query_type
   end
-    
+
   def parseCoordinates(key)
     # This regular expression should probably handle any kind of mess. Thanks sbrynen!
     re = /^([ns-]?)\s*([\d\.]+)\W*([\d\.]*)[\s,]+([ew-]?)\s*([\d\.]+)\W*([\d\.]+)/i
-    md = re.match(key)            
+    md = re.match(key)
     if ! md
       displayError "Bad format in #{key}! Try something like \"N56 44.392 E015 52.780\" instead"
     end
-          
+
     lat_dir = md[1]
     lat_h = md[2]
-    lat_ms = md[3]            
+    lat_ms = md[3]
     long_dir = md[4]
     long_h = md[5]
-    long_ms = md[6]            
+    long_ms = md[6]
     lat_ns = 1
     long_ew = 1
-          
+
     if lat_dir == 's' || lat_dir == 'S' || lat_dir == '-'
       lat_ns = -1
     end
-          
+
     if long_dir == 'w' || long_dir == 'W' || long_dir == '-'
       long_ew = -1
     end
     displayMessage "Coordinate's have been parsed as latitude #{lat_dir} #{lat_h}'#{lat_ms}, longitude #{long_dir} #{long_h}'#{long_ms}"
     coords = [lat_dir, lat_h, lat_ms, long_dir, long_h, long_ms, lat_ns, long_ew]
     return coords
-  end    
-        
+  end
+
   def getResults()
     debug "Getting results: #{@query_type} at #{@search_url}"
     if @query_type == 'wid'
@@ -124,18 +124,18 @@ class SearchCache
       'guid' => guid,
       'disabled' => false,
       'archived' => false,
-      'membersonly' => false      
+      'membersonly' => false
     }
     return cache_data
   end
-  
+
   def searchResults(url)
     debug "searchResults: #{url}"
     if not url
       warn "searchResults has no URL?"
     end
     post_vars = Hash.new
-        
+
     page_number, pages_total, parsed_total, post_vars, src = processPage({})
     progress = ProgressBar.new(1, pages_total, "Processing results for #{@query_arg}")
     progress.updateText(page_number, "from #{src}")
@@ -143,23 +143,23 @@ class SearchCache
       displayMessage "No geocaches were found."
       return @waypoints
     end
-    
+
     if not page_number
       displayError "Could not determine current page number from #{url}"
       return @waypoints
     end
-    
+
     if not pages_total
       displayError "Could not determine total pages from #{url}"
       return @waypoints
     end
-      
+
 
     while(page_number < pages_total)
       debug "*** On page #{page_number} of #{pages_total}"
       last_page_number = page_number
       page_number, total_pages, total_waypoints, post_vars, src = processPage(post_vars)
-      progress.updateText(page_number, "from #{src}")      
+      progress.updateText(page_number, "from #{src}")
       if (src == "remote")
         debug "sleeping"
         sleep($SLEEP)
@@ -172,21 +172,21 @@ class SearchCache
     end
     return @waypoints
   end
-    
+
   def getPage(url, post_vars)
     page = ShadowFetch.new(url)
     page.localExpiry = @ttl
     if (post_vars.length > 0)
       page.postVars=post_vars.dup
     end
-      
+
     if (page.fetch)
       return page.data
     else
       return nil
     end
   end
-  
+
   def processPage(post_vars)
     data = getPage(@search_url, post_vars)
     page_number, pages_total, parsed_total, post_vars = parseSearchData(data)
@@ -194,8 +194,8 @@ class SearchCache
     src = cache_check.src
     return [page_number, pages_total, parsed_total, post_vars, src]
   end
-  
-      
+
+
   def parseSearchData(data)
     page_number = nil
     pages_total = nil
@@ -219,7 +219,7 @@ class SearchCache
           page_number = $2.to_i
           pages_total = $3.to_i
         end
-        # href="javascript:__doPostBack('ctl00$ContentBody$pgrTop$ctl08','')"><b>Next &gt;</b></a></td> 
+        # href="javascript:__doPostBack('ctl00$ContentBody$pgrTop$ctl08','')"><b>Next &gt;</b></a></td>
         # <a href="javascript:__doPostBack('pgrTop$_ctl16','')"><b>Next</b></a>
         if line =~ /doPostBack\(\'([\w\$_]+)\',\'\'\)\"\>\<b\>Next/
           debug "Found next target: #{$1}"
@@ -231,7 +231,7 @@ class SearchCache
       when /WptTypes\/[\w].*?alt=\"(.*?)\"/
         # This line also has travel bug data
         full_type = $1
-        if line =~ /Travel Bug.*?\((.*?)\)/ 
+        if line =~ /Travel Bug.*?\((.*?)\)/
           debug "Travel Bug Found: #{$1}"
           cache['travelbug']=$1
         else
@@ -243,7 +243,7 @@ class SearchCache
         cache['fulltype']=full_type
         cache['type']=short_type
         debug "type=#{cache['type']}"
-        
+
         # This line also has travel bug data
         if line =~ /Travel Bug.*?(\d+) item"/
           debug "Travel Bug Found: #{$1}"
@@ -258,7 +258,7 @@ class SearchCache
           cache['difficulty']=$1.to_i
         else
           cache['difficulty']=$1.to_f
-        end 
+        end
 
         if $1.to_f == $1.to_i
           cache['terrain']=$2.to_i
@@ -266,31 +266,31 @@ class SearchCache
           cache['terrain']=$2.to_f
         end
         debug "difficulty=#{cache['difficulty']} terr=#{cache['terrain']}"
-        
+
       # <img src="/images/icons/container/micro.gif" alt="Size: Micro" />
       when /\<img src=\"\/images\/icons\/container\/.*\" alt=\"Size: (.*?)\"/
         cache['size'] = $1.downcase
         debug "size=#{cache['size']}"
 
-      #                             11 Jul 10<br /> 
+      #                             11 Jul 10<br />
       when /^ (\w+[ \w]+)\**\<[bs][rt]/
         debug "last found date: #{$1} at line: #{line}"
         cache['mtime'] = parseDate($1)
         cache['mdays'] = daysAgo(cache['mtime'])
         debug "mtime=#{cache['mtime']} mdays=#{cache['mdays']}"
-        
+
       #  9 Sep 06
-      when /^ +(\d+ \w{3} \d+).*/
+      when /^ +(\d+ \w{3} \d+)\s+$/
         cache['ctime'] = parseDate($1)
         cache['cdays'] = daysAgo(cache['ctime'])
         debug "ctime=#{cache['ctime']} cdays=#{cache['cdays']}"
-        
+
       # <img src="/images/icons/compass/SW.gif" alt="SW" />SW<br />0.9mi</td>
       when /([NWSE]+)\<br \/\>([\d\.]+)mi\</
         cache['distance']=$2.to_f
         cache['direction'] = $1
         debug "cacheDistance=#{cache['distance']} dir=#{cache['direction']}"
-      
+
       # <a href="/seek/cache_details.aspx?guid=c9f28e67-5f18-45c0-90ee-76ec8c57452f">Yasaka-Shrine@Zerosen</a>
       when /cache_details.aspx\?guid=(.*?)\">(.*?)\<\/a\>/
         cache['guid']=$1
@@ -320,7 +320,7 @@ class SearchCache
         creator = $1
         cache['creator'] = creator.gsub(/\s+$/, '')
         debug "creator=#{cache['creator']}"
-      
+
       # (GC1Z0RT)<br />
       when /^ +\((GC.*?)\)\<br \/\>/
         wid = $1
@@ -330,7 +330,7 @@ class SearchCache
       when /Premium Member Only Cache/
         debug "#{wid} is a members only cache. Marking"
         cache['membersonly'] = true
-                                  
+
       when /^\s+<\/tr\>/
         debug "--- end of row ---"
         if wid and not @waypoints.has_key?(wid)
@@ -343,7 +343,7 @@ class SearchCache
 
           @waypoints[wid] = cache.dup
           @waypoints[wid]['visitors'] = []
-                      
+
           # if our search is for caches that have been done by a particular user, add to the hash
           if @query_type == "users"
             @waypoints[wid]['visitors'].push(@key.downcase)
@@ -354,10 +354,10 @@ class SearchCache
       when /^\<input type=\"hidden\" name=\"(.*?)\".*value=\"(.*?)\" \/\>/
         debug "found hidden post variable: #{$1}"
         post_vars[$1]=$2
-                
+
       end # end case
     } # end loop
     debug "processPage done: page:#{page_number} total_pages: #{pages_total} parsed: #{parsed_total}"
     return [page_number, pages_total, parsed_total, post_vars]
-  end #end parsecache        
+  end #end parsecache
 end
