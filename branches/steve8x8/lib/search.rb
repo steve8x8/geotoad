db49775e10e50483cd2911d527d5cf17a31db09b
Steve8x8@gmail.com <Steve8x8@gmail.com@abbcce6f-711e-0410-a2ab-651830b4d790>
2010-11-17 10:50:26 +0000

    Minor trackables cleanup (do not show "(1)" counts). Detect US states, and set country/state accordingly.

--- branches/steve8x8/lib/search.rb
+++ branches/steve8x8/lib/search.rb
@@ -209,6 +209,60 @@ class SearchCache
       'archived' => false,
       'membersonly' => true
     }
+    # list of US states, generated from seek page
+    usstates = {
+      "Alabama" => 60,
+      "Alaska" => 2,
+      "Arizona" => 3,
+      "Arkansas" => 4,
+      "California" => 5,
+      "Colorado" => 6,
+      "Connecticut" => 7,
+      "Delaware" => 9,
+      "District of Columbia" => 8,
+      "Florida" => 10,
+      "Georgia" => 11,
+      "Hawaii" => 12,
+      "Idaho" => 13,
+      "Illinois" => 14,
+      "Indiana" => 15,
+      "Iowa" => 16,
+      "Kansas" => 17,
+      "Kentucky" => 18,
+      "Louisiana" => 19,
+      "Maine" => 20,
+      "Maryland" => 21,
+      "Massachusetts" => 22,
+      "Michigan" => 23,
+      "Minnesota" => 24,
+      "Mississippi" => 25,
+      "Missouri" => 26,
+      "Montana" => 27,
+      "Nebraska" => 28,
+      "Nevada" => 29,
+      "New Hampshire" => 30,
+      "New Jersey" => 31,
+      "New Mexico" => 32,
+      "New York" => 33,
+      "North Carolina" => 34,
+      "North Dakota" => 35,
+      "Ohio" => 36,
+      "Oklahoma" => 37,
+      "Oregon" => 38,
+      "Pennsylvania" => 39,
+      "Rhode Island" => 40,
+      "South Carolina" => 41,
+      "South Dakota" => 42,
+      "Tennessee" => 43,
+      "Texas" => 44,
+      "Utah" => 45,
+      "Vermont" => 46,
+      "Virginia" => 47,
+      "Washington" => 48,
+      "West Virginia" => 49,
+      "Wisconsin" => 50,
+      "Wyoming" => 51,
+    }
 
     data.split("\n").each { |line|
       # GC change 2010-11-09
@@ -265,12 +319,13 @@ class SearchCache
           # "Unite for Diabetes Travel Bug (1 item(s))" ...
           item.gsub!(/\".*/, '')
           # shorten the name a bit
+          item.gsub!(/^Geocoins:\s+/, '')
           item.gsub!(/Travel Bug( Dog Tag)?/, 'TB')
           item.gsub!(/Geocoin/, 'GC')
           item.gsub!(/^The /, '')
           # drop counter if 1
-          item.gsub!(/ \(1 item\(?s?\)?\)/, '')
           item.gsub!(/ \((.*?) item\(?s?\)?\)/) {"(#{$1})"}
+          item.gsub!(/\(1\)/, '')
           trackables << item + ', '
         }
         if trackables.length > 0
@@ -382,8 +437,14 @@ class SearchCache
       when /^\s{28}((.*?), )?([A-Z][a-z]+\.?([ -]\(?[A-Za-z]+\)?)*)\s?$/
         debug "COUNTRY STATE 2=#{$2} 3=#{$3}"
         if ($3 != "Icons" && $3 != "Placed" && $3 != "Description" && $3 != "Last Found")
-          cache['country'] = $3
-          cache['state'] = ($2)?$2:'-' # GCStatistic doesn't like empty state elements
+          # special case US states:
+          if (usstates[$3])
+            cache['country'] = 'United States'
+            cache['state'] = $3
+          else
+            cache['country'] = $3
+            cache['state'] = ($2)?$2:'-' # GCStatistic doesn't like empty state elements
+          end
           debug "COUNTRY #{cache['country']} STATE #{cache['state']}"
         end
 
