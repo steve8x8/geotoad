f0acba121c257a68b52057e72ff39a6dc300e0b2
Steve8x8@gmail.com <Steve8x8@gmail.com@abbcce6f-711e-0410-a2ab-651830b4d790>
2010-11-18 17:10:57 +0000

    Parse country (and state, where applicable) from search page. Special case of US states properly handled, as are special characters in state/province/... names (country is always English).

--- trunk/lib/search.rb
+++ trunk/lib/search.rb
@@ -209,6 +209,60 @@ class SearchCache
       'archived' => false,
       'membersonly' => true
     }
+    # list of US states, generated from seek page
+    usstates = {
+      "Alabama" => 60,
+      "Alaska" => 2,
+      "Arizona" => 3,
+      "Arkansas" => 4,
+      "California" => 5,
+      "Colorado" => 6,
+      "Connecticut" => 7,
+      "Delaware" => 9,
+      "District of Columbia" => 8,
+      "Florida" => 10,
+      "Georgia" => 11,
+      "Hawaii" => 12,
+      "Idaho" => 13,
+      "Illinois" => 14,
+      "Indiana" => 15,
+      "Iowa" => 16,
+      "Kansas" => 17,
+      "Kentucky" => 18,
+      "Louisiana" => 19,
+      "Maine" => 20,
+      "Maryland" => 21,
+      "Massachusetts" => 22,
+      "Michigan" => 23,
+      "Minnesota" => 24,
+      "Mississippi" => 25,
+      "Missouri" => 26,
+      "Montana" => 27,
+      "Nebraska" => 28,
+      "Nevada" => 29,
+      "New Hampshire" => 30,
+      "New Jersey" => 31,
+      "New Mexico" => 32,
+      "New York" => 33,
+      "North Carolina" => 34,
+      "North Dakota" => 35,
+      "Ohio" => 36,
+      "Oklahoma" => 37,
+      "Oregon" => 38,
+      "Pennsylvania" => 39,
+      "Rhode Island" => 40,
+      "South Carolina" => 41,
+      "South Dakota" => 42,
+      "Tennessee" => 43,
+      "Texas" => 44,
+      "Utah" => 45,
+      "Vermont" => 46,
+      "Virginia" => 47,
+      "Washington" => 48,
+      "West Virginia" => 49,
+      "Wisconsin" => 50,
+      "Wyoming" => 51,
+    }
 
     data.split("\n").each { |line|
       # GC change 2010-11-09
@@ -371,6 +425,28 @@ class SearchCache
       when /^ +\((GC.*?)\)\<br \/\>/
         wid = $1
         debug "wid=#{wid}"
+
+      # country/state: prefixed by 28 blanks
+      # Mecklenburg-Vorpommern, Germany
+      # East Midlands, United Kingdom
+      # Comunidad Valenciana, Spain
+      # North Carolina (will be interpreted as country, not U.S. state!)
+      # also valid (country only):
+      # Croatia; Isle of Man; Bosnia and Herzegovina, St. Martin, Guinea-Bissau; Cocos (Keeling) Islands
+      #             |>$2|    |->$3 -------------------------------|
+      when /^\s{28}((.*?), )?([A-Z][a-z]+\.?([ -]\(?[A-Za-z]+\)?)*)\s?$/
+        debug "COUNTRY STATE 2=#{$2} 3=#{$3}"
+        if ($3 != "Icons" && $3 != "Placed" && $3 != "Description" && $3 != "Last Found")
+          # special case US states:
+          if (usstates[$3])
+            cache['country'] = 'United States'
+            cache['state'] = $3
+          else
+            cache['country'] = $3
+            cache['state'] = ($2)?$2:'-' # GCStatistic doesn't like empty state elements
+          end
+          debug "COUNTRY #{cache['country']} STATE #{cache['state']}"
+        end
 
       # small_profile.gif" alt="Premium Member Only Cache" with="15" height="13"></TD
       when /Premium Member Only Cache/
