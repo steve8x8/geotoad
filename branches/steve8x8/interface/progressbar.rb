ef488404f5559fb9ec543cbbd1aab6f368a99dd2
Steve8x8@gmail.com <Steve8x8@gmail.com@abbcce6f-711e-0410-a2ab-651830b4d790>
2010-11-29 13:42:27 +0000

    More detailed progress bar (25 states)

--- branches/steve8x8/interface/progressbar.rb
+++ branches/steve8x8/interface/progressbar.rb
@@ -7,48 +7,55 @@ class ProgressBar
     @name = name
     display
   end
-    
+
   def update(value)
     @value = value
   end
-    
+
   def setName(name)
     @name = name
   end
-    
+
   def updateText(value, valueText)
     @value = value
     @valueText = valueText
     display
   end
-    
+
   def setMax(value)
     @max = max
   end
-    
+
   def display
     if (@max == 0)
       showMax = "?"
     else
       showMax = @max.to_s
     end
-        
+
     # if the value is 0 or less, don't bother to print up a bar.
     if not @value or @value < 1
       return
     end
 
+    # adjust the definition of metercols and meterchar to actual needs
+    # number of columns
+    metercols = 3
+    # available character set, with increasing black density
+    meterchar = ' .-+x*=%#'		# [0..7] for mini, [8] for full
+    minichars = meterchar.length - 1	# 8
     if @value == @max
-      meter = '==='
+      meter = meterchar[-1,1] * metercols
     else
-      percentage = (@value.to_f / @max.to_f) * 100
-      bardiv = percentage.divmod(33.333333333333333333333333333333333333)
-        
+      # map 0..100% to 0..24 different states
+      percentage = (@value.to_f / @max.to_f) * metercols * minichars
+      # divmod by 8 gives full thirds and remainder
+      bardiv = percentage.divmod(minichars)
       fullbars = bardiv[0]
-      minibars = bardiv[1] / (16.66666666666666666666666666667).round
-      # "p=#{percentage} f=#{fullbars} m=#{minibars}"
-      meter = "=" * fullbars + "-" * minibars
-      meter = meter.ljust(3)
+      minibars = bardiv[1]
+      #debug "#{percentage} -> #{fullbars}*full+#{minibars}"
+      meter = meterchar[-1,1] * fullbars + meterchar[minibars,1]
+      meter = meter.ljust(metercols)
     end
 
     if (@valueText)
