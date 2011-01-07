e03b8df28f3f5ff2ccf822cd605dfafd533271f4
Steve8x8@gmail.com <Steve8x8@gmail.com@abbcce6f-711e-0410-a2ab-651830b4d790>
2011-01-07 09:18:00 +0000

    Create geocache_visits.txt (issue 188)

--- lib/templates.rb
+++ lib/templates.rb
@@ -489,4 +489,26 @@ N|O|P|Q|R|S|T|U|V|W|X|Y|Z</pre></font><br>",
       "<%out.mdate%> " +
       "\n"
   },
+
+  # mygeocachelist (law.skynet) - use as follows:
+  # geotoad -x gclist -o geocache_list.txt -z -q user $USERNAME
+  'gclist'    => {
+    'ext'     => 'txt',
+    'mime'    => 'text/plain',
+    'desc'    =>     'Geocache visits text file for Garmin devices',
+    'templatePre' => "",
+    'templateWP'  => "<%out.wid%>,<%out.mdate%>T08:00Z,Found it,\"\"\r\n"
+  },
+
+  # mygeocachevisits (law.skynet) - use as follows:
+  # geotoad -x gcvisits -o geocache_visits.txt -z -q user $USERNAME
+  'gcvisits' => {
+    'ext'        => 'txt',
+    'mime'    => 'text/plain',
+    'required' => 'iconv',
+    'desc'    => 'Geocache visits Unicode file for Garmin devices',
+    'filter_src'    => 'gclist',
+    'filter_exec'    => 'iconv -f US-ASCII -t UCS-2LE INFILE > OUTFILE'
+  },
+
 }
