4404cb47f23642b098b9506a0263a26c238cfcda
Steve8x8@gmail.com <Steve8x8@gmail.com@abbcce6f-711e-0410-a2ab-651830b4d790>
2011-06-19 17:35:47 +0000

    TUI: If query arg starts with '-', insert '--' into suggested cmd line (issue 208)

--- interface/input.rb
+++ interface/input.rb
@@ -197,6 +197,9 @@ class Input
       end
 
     }
+    if @@optHash['queryArg'].to_s[0] == "-"
+      cmdline << "--"
+    end
     if @@optHash['queryArg'].to_s =~ /^[\w\.:]+$/
       cmdline = cmdline + " " + @@optHash['queryArg'].to_s
     else
