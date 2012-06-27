7b48ada3732636fcd6eaac731b3f11e653e78307
Steve8x8@gmail.com <Steve8x8@gmail.com@abbcce6f-711e-0410-a2ab-651830b4d790>
2012-06-27 09:27:03 +0000

    Minor stuff

--- trunk/lib/filter.rb
+++ trunk/lib/filter.rb
@@ -172,11 +172,10 @@ class Filter
   end
 
   def userExclude(nick)
-    nick.downcase!
     debug "filtering by notUser: #{nick}"
     @waypointHash.each_key { |wid|
       debug "#{wid} visitors: #{@waypointHash[wid]['visitors']}"
-      if (@waypointHash[wid]['visitors'].include?(nick))
+      if (@waypointHash[wid]['visitors'].include?(nick.downcase))
         debug " - #{nick} has visited #{wid} #{@waypointHash[wid]['name']}, filtering."
         @waypointHash.delete(wid)
       end
@@ -184,11 +183,10 @@ class Filter
   end
 
   def userInclude(nick)
-    nick.downcase!
     debug "filtering by User: #{nick}"
     @waypointHash.each_key { |wid|
       debug "#{wid} visitors: #{@waypointHash[wid]['visitors']}"
-      if (! @waypointHash[wid]['visitors'].include?(nick))
+      if (! @waypointHash[wid]['visitors'].include?(nick.downcase))
         debug " - #{nick} has not visited #{@waypointHash[wid]['name']}, filtering."
         @waypointHash.delete(wid)
       end
