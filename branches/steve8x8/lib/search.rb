d268b6ce2b739a82f89ba9f9022394e3199f3edb
Steve8x8@gmail.com <Steve8x8@gmail.com@abbcce6f-711e-0410-a2ab-651830b4d790>
2012-10-13 10:27:50 +0000

    Issue 257: query countries divided into states (provinces, departements etc) by adding "as=1" (as in "all states")

--- branches/steve8x8/lib/search.rb
+++ branches/steve8x8/lib/search.rb
@@ -152,7 +152,7 @@ class SearchCache
 
     when 'country'
       @query_type = 'country'
-      @search_url = @@base_url + "?country_id=#{key}"
+      @search_url = @@base_url + "?country_id=#{key}&as=1"
       @ttl = 14 * 24 * 3600	# 2 weeks
 
     when 'state'
