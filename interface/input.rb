5e4294ecbd23518cd390c08dce7c75996c710a55
Steve8x8@googlemail.com <Steve8x8@googlemail.com@abbcce6f-711e-0410-a2ab-651830b4d790>
2013-12-17 19:10:36 +0000

    A few minor fixes.

--- interface/input.rb
+++ interface/input.rb
@@ -550,7 +550,7 @@ class Input
         @@optHash['ownerInclude'] = ask('Only include geocaches owned by these people (separate by commas)', '').gsub(/, */, '|')
 
       when '20'
-        @@optHash['waypointLength'] = askNumber('How long can your EasyName waypoint id\'s be? (8 for Magellan, 16 for Garmin, -1 to use full text, 0 to disable and use waypoint id\'s)?', nil, true)
+        @@optHash['waypointLength'] = askNumber('How long can your EasyName waypoint id\'s be? (recommended: 8 for Magellan, 16 for Garmin, 0 to disable and use waypoint id\'s)?', nil) #, true)
 
       when '21'
         answer = ask('Include disabled caches in your results?', nil)
@@ -652,7 +652,7 @@ class Input
     return answer
   end
 
-  def askNumber(string, default, allowNegative = false)
+  def askNumber(string, default) #, allowNegative=false)
     # Ask for a floating point number. Only accept non-negative values.
     while true
       begin
@@ -663,7 +663,7 @@ class Input
           # this may throw an ArgumentError
           answerf = Float(answer)
           # negative values aren't allowed
-          if (answerf < 0) and (not allowNegative)
+          if (answerf < 0) #and (not allowNegative)
             puts "** #{answer} is negative, not allowed."
           # If it is equivalent to it's integer, return the integer instead
           elsif answerf == answerf.to_i
