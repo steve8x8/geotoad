ca7f0a9bb6049b31921d8f029c5dd6ec93ef9bb8
Steve8x8@googlemail.com <Steve8x8@googlemail.com@abbcce6f-711e-0410-a2ab-651830b4d790>
2014-08-17 09:11:23 +0000

    Remove <font> tags. Also font-size, [background-]color don't make much sense.

--- lib/output.rb
+++ lib/output.rb
@@ -649,6 +649,13 @@ class Output
     #text.gsub!(/(<\/a.*?>)/im){"#{$1}[/link]"}
     text.gsub!(/<\/a.*?>/im){"[=]"}
 
+    # fonts are not represented properly on most devices
+    # avoid huge sizes, dark on black, white on white
+    text.gsub!(/<\/?font[^>]*>/im, '')
+    # also for style=...
+    text.gsub!(/([;\'\"])\s*(background-)?color:[^;]*;/){$1}
+    text.gsub!(/font-size:\d+p/, 'font-size:12p') # can be pt or px...
+
     # escape HTML entities (including <>)
     begin
         text = CGI.escapeHTML(text)
