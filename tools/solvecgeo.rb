5c126a87c0d3c90e4c293a11b5104f61a4fa2ee4
Steve Sixty-Four <steve8x8@googlemail.com>
2017-01-29 17:00:43 +0100

    Issue 347: Do something about corrected coordinates. Use solvecgeo.rb!

--- tools/solvecgeo.rb
+++ tools/solvecgeo.rb
@@ -1,81 +1,90 @@
 #!/usr/bin/env ruby
+
 # append waypoints to GPX file
 # do not replace cache coordinates!
 # better suited for c:geo
 
-#read waypoints (WPID lat lon) from table
+# read waypoints (WPID lat lon [comment...]) from table
 waypoints = Hash.new
+
 begin
 File.foreach(ARGV[0]) { |line|
-    columns = line.upcase.split(/\s+/)
-    name = columns[0]
-    lat  = columns[1]
-    lon  = columns[2]
-    comm = columns[3]
-    wpnr = name[0..1]
-    wpid = name[0..-1]
-    case wpnr
-      when /GC/
-	type = 'Final Location'
-	wpnr = '99'
-	
-      when /\d./
-	type = 'Trailhead'
-	wpnr.gsub!(/^0/, 'X')
-    end
-    waypoints[wpid] = {'wpnr'=>wpnr, 'lat'=>lat, 'lon'=>lon, 'type'=>type, 'comm'=>comm}
+  columns = line.split(/\s+/)
+  name    = columns[0].upcase
+  lat     = columns[1]		# sDD.DDDDDD
+  lon     = columns[2]		# sDD.DDDDDD
+  comment = columns[3..-1].join(" ")	# may be empty
+  wpnr    = name[0..1]		# waypoint prefix & type
+  case wpnr
+    # final location
+    when /GC/
+      type = 'Final Location'
+      wpnr = '99'
+    # numbers: multi stages
+    when /\d./
+      # should be 'Virtual Stage' or 'Physical Stage'
+      # but how to identify?
+      type = 'Trailhead'
+      wpnr.gsub!(/^0/, 'X').gsub!(/^1/, 'Y').gsub!(/^2/, 'Z')
+    # extend here
+    when /R./
+      type = 'Reference Point'
+      wpnr.gsub!(/^R/, 'Q')
+  end
+  waypoints[name] = {'wpnr' => wpnr, 'lat' => lat, 'lon' => lon, 'type' => type, 'comm' => comment}
 }
 rescue
-    $stderr.puts "Cannot read #{ARGV[0].inspect}, will not modify anything"
+  $stderr.puts "Cannot read #{ARGV[0].inspect}, will not modify anything"
 end
 
+# read whole gpx file
 fulltext = $stdin.read
 wpttext = ""
-wpids = Array.new
 fulltext.split(/<\/wpt>/).each { |fragment|
-    if fragment =~ /<name>(GC.*?)<\/name>/m
-	gcid = $1
-	waypoints.each_key{ |wpid|
-	    # is this a waypoint for this cache?
-	    if gcid[2..-1] == wpid[2..-1]
-		# append waypoint info
-		waypoint = waypoints[wpid]
-		wpnr = waypoint['wpnr']
-		lat  = waypoint['lat']
-		lon  = waypoint['lon']
-		type = waypoint['type']
-		comm = waypoint['comm']
-		wpttext += "" +
-		    "<wpt lat=\"#{lat}\" lon=\"#{lon}\">\n" +
-		    "  <name>#{wpnr}#{wpid[2..-1]}</name>\n" +
-		    "  <desc>#{type} #{wpnr}</desc>\n" +
-		    "  <cmt>#{comm}</cmt>\n" +
-		    "  <sym>#{type}</sym>\n" +
-		    "  <type>Waypoint|#{type}</type>\n" +
-		    "  <gsak:wptExtension>\n" +
-		    "    <gsak:Parent>GC#{wpid[2..-1]}</gsak:Parent>\n" +
-		    "  </gsak:wptExtension>\n" +
-# this breaks c:geo import!
-#		    "  <cgeo:userdefined>true</cgeo:userdefined>\n" +
-		    "</wpt>\n"
-		$stderr.printf "Modified %7s: %s = %s %s -> %s\n", gcid, wpnr, lat, lon, type
-		wpids.push wpid
-	    end
-	}
-    end
+  # walk through all cache sections in gpx file
+  if fragment =~ /<name>(GC.*?)<\/name>/m
+    gcid = $1
+    # check whole list of wayppints read before
+    # there may be more than one matching entry!
+    waypoints.each_key{ |wpid|
+      # is this a waypoint for this cache?
+      if gcid[2..-1] == wpid[2..-1]
+        # append waypoint info
+        waypoint = waypoints[wpid]
+        wpnr     = waypoint['wpnr']
+        lat      = waypoint['lat']
+        lon      = waypoint['lon']
+        type     = waypoint['type']
+        comment  = waypoint['comm']
+        wpttext += "" +
+          "<wpt lat=\"#{lat}\" lon=\"#{lon}\">\n" +
+          "  <name>#{wpnr}#{wpid[2..-1]}</name>\n" +
+          "  <desc>#{type} #{wpnr}</desc>\n" +
+          "  <cmt>#{comment}</cmt>\n" +
+          "  <sym>#{type}</sym>\n" +
+          "  <type>Waypoint|#{type}</type>\n" +
+          "  <gsak:wptExtension>\n" +
+          "    <gsak:Parent>GC#{wpid[2..-1]}</gsak:Parent>\n" +
+          "  </gsak:wptExtension>\n" +
+          "</wpt>\n"
+        $stderr.printf "Modified %7s: %s = %s %s -> %s (%s)\n", gcid, wpnr, lat, lon, type, comment
+      end
+    }
+  end
 }
+# insert collected waypoints right before closing <gpx> tag
 $stdout.puts fulltext.gsub(/<\/gpx>/, wpttext + "</gpx>")
 
 # to be run as follows:
 #
-#   solvecgeo.sh solved.txt <input.gpx >output.gpx
+#   solvecgeo.rb solved.txt <input.gpx >output.gpx
 #
 # with a solved.txt file listing points by name, lat, lon (where point names are 
 # constructed the same way as "normal" waypoints) as in the following fictional list:
 #
-# gcqmea  02.012345 53.987654
-# gcwpgf  13.543210 41.456789
-# 0113r5p 22.555555 33.444444
-# 0116d4t 32.888888 23.555555
-# 0018k6k 42.010101 13.101010
-# 0118k6k 42.020202 13.202020
+# gcqmea  02.012345 53.987654 possible multi-field comment
+# gcwpgf  13.543210 41.456789 use 4711 to unlock
+# 0113r5p 22.555555 33.444444 back side of tree
+# 0116d4t 32.888888 23.555555 T4!
+# 0018k6k 42.010101 13.101010 park here
+# 0118k6k 42.020202 13.202020 trailhead
--- tools/solvemyst.rb
+++ tools/solvemyst.rb
@@ -1,61 +1,64 @@
 #!/usr/bin/env ruby
-# merge solved mysteries into GPX file
-# ToDo: keep old waypoint (how?)
 
-# first read solutions (WPID lat lon) from table
+# merge solved mysteries into GPX file, overwriting
+# ToDo: keep old waypoint (how?)
+# caveat: there can be only one, last matching entry wins
+
+# read solutions (WPID lat lon [comment...]) from table
 solutions = Hash.new
+
 begin
 File.foreach(ARGV[0]) { |line|
-    columns = line.upcase.split(/\s+/)
-    name = columns[0]
-    lat = columns[1]
-    lon = columns[2]
-    gcid = "GC" + name[2..-1]
-    wpnr = name[0..1]
-    case wpnr
+  columns = line.split(/\s+/)
+  name    = columns[0].upcase
+  lat     = columns[1]
+  lon     = columns[2]
+  comment = columns[3..-1].join(" ")
+  gcid    = "GC" + name[2..-1]
+  wpnr    = name[0..1]
+  case wpnr
     when /GC/
-	type = 'Traditional Cache'
+      type = 'Traditional Cache'
     when /\d./
-	type = 'Multi-cache'
+      type = 'Multi-cache'
     end
-    solutions[gcid] = {'wpnr'=>wpnr, 'lat'=>lat, 'lon'=>lon, 'type'=>type}
+    solutions[gcid] = {'wpnr' => wpnr, 'lat' => lat, 'lon' => lon, 'type' => type, 'comm' => comment}
 }
 rescue
-    $stderr.puts "Cannot read #{ARGV[0].inspect}, will not modify anything"
+  $stderr.puts "Cannot read #{ARGV[0].inspect}, will not modify anything"
 end
 
 # now merge into GPX (UNIX filter!)
 puts $stdin.read.split(/<\/wpt>/).map { |fragment|
-    if fragment =~ /<name>(GC.*?)<\/name>.*?<groundspeak:type>(Unknown Cache|Multi-cache)/m
-	gcid = $1
-	if fragment =~ /<groundspeak:name>(.*?)</m
-	    name = $1
-	end
-	solution = solutions[gcid]
-	if solution
-	    wpnr = solution['wpnr']
-	    lat = solution['lat']
-	    lon = solution['lon']
-	    type = solution['type']
-	    fragment.gsub!(/lat="[\d\.-]+"/, "lat=\"#{lat}\"")
-	    fragment.gsub!(/lon="[\d\.-]+"/, "lon=\"#{lon}\"")
-	    fragment.gsub!(/(Unknown Cache|Multi-cache)</, "#{type}<")
-	    fragment.gsub!(/<groundspeak:short_description[^>]*>/) { |s| s.gsub(/>/, ">[+#{wpnr}]=") }
-	    $stderr.printf "Modified %7s: %s = %s %s \"%s\" -> %s\n", gcid, wpnr, lat, lon, name, type
-	end
+  if fragment =~ /<name>(GC.*?)<\/name>.*?<groundspeak:type>(Unknown Cache|Multi-cache)/m
+    gcid = $1
+    solution = solutions[gcid]
+    if solution
+      wpnr    = solution['wpnr']
+      lat     = solution['lat']
+      lon     = solution['lon']
+      type    = solution['type']
+      comment = solution['comm']
+      fragment.gsub!(/lat="[\d\.-]+"/, "lat=\"#{lat}\"")
+      fragment.gsub!(/lon="[\d\.-]+"/, "lon=\"#{lon}\"")
+      fragment.gsub!(/(Unknown Cache|Multi-cache)</, "#{type}<")
+      fragment.gsub!(/<groundspeak:short_description[^>]*>/) { |s| s.gsub(/>/, ">[+#{wpnr}(#{comment})]=") }
+      $stderr.printf "Modified %7s: %s = %s %s -> %s (%s)\n", gcid, wpnr, lat, lon, type, comment
     end
-    fragment
+  end
+  fragment
 }.join("</wpt>")
 
 # to be run as follows:
 #
-#   solvemyst.sh solved.txt <input.gpx >output.gpx
+#   solvemyst.rb solved.txt <input.gpx >output.gpx
 #
 # with a solved.txt file listing points by name, lat, lon (where point names are 
 # constructed the same way as "normal" waypoints) as in the following fictional list:
 #
-# gcqmea  02.012345 53.987654
-# gcwpgf  13.543210 41.456789
-# 0113r5p 22.555555 33.444444
-# 0116d4t 32.888888 23.555555
-# 0018k6k 42.010101 13.101010
+# gcqmea  02.012345 53.987654 possible multi-field comment
+# gcwpgf  13.543210 41.456789 use 4711 to unlock
+# 0113r5p 22.555555 33.444444 back side of tree
+# 0116d4t 32.888888 23.555555 T4!
+# 0018k6k 42.010101 13.101010 park here
+# 0118k6k 42.020202 13.202020 trailhead
