steve8x8/geotoad

commit 7714b78228558cf8ef5dea52771cc17491de4a7c

Classes cleanup: more consistent handling of ShadowFetch @src. fetchWid() now returns status and src.

lib/search.rb
@@ -592,18 +592,16 @@ class SearchCache
       page.postVars = post_vars.dup
     end
 
-    if (page.fetch)
-      return page.data
+    if page.fetch
+      return [page.data, page.src]
     else
-      return nil
+      return [nil, nil]
     end
   end
 
   def processPage(post_vars)
-    data = getPage(@search_url, post_vars)
+    data, src = getPage(@search_url, post_vars)
     page_number, pages_total, parsed_total, post_vars = parseSearchData(data)
-    cache_check = ShadowFetch.new(@url)
-    src = cache_check.src
     return [page_number, pages_total, parsed_total, post_vars, src]
   end
 

geotoad.rb
@@ -716,10 +716,8 @@ class GeoToad
     token = 0
 
     wpFiltered.each_key{ |wid|
-      token = token + 1
-      detailURL = @detail.fullURL(wid)
-      page = ShadowFetch.new(detailURL)
-      status = @detail.fetch(wid)
+      token += 1
+      status, src = @detail.fetchWid(wid)
       message = nil
 
       if status == 'login-required'
@@ -773,8 +771,7 @@ class GeoToad
       end
       name = temp
       message << (keepdata ? "" : "(del)")
-#      progress.updateText(token, "[#{wid}]".ljust(9)+" \"#{name}\" (#{page.src.gsub(/(\w)\w*/){$1}}) #{message}")
-      progress.updateText(token, "[#{wid}]".ljust(9)+" \"#{name}\" (#{page.src}) #{message}")
+      progress.updateText(token, "[#{wid}]".ljust(9)+" \"#{name}\" (#{src}) #{message}")
 
       if not keepdata
         debug "Page for #{wid} \"#{wpFiltered[wid]['name']}\" failed to be parsed, invalidating cache."

lib/details.rb
@@ -68,10 +68,10 @@ class CacheDetails
     @@baseURL
   end
 
-  # this routine is for compatibility.
-  def fetchWid(id)
-    fetch(id)
-  end
+#  # this routine is for compatibility.
+#  def fetchWid(id)
+#    fetch(id)
+#  end
 
   def getRemoteMapping(wid)
     # get guid via GC code lookup
@@ -151,17 +151,15 @@ class CacheDetails
   end
 
   # fetches by geocaching.com sid
-  def fetch(id)
+  def fetchWid(id)
     if id.to_s.empty?
-      displayError "Empty fetch by id, quitting."
+      displayError "Empty fetch by wid, quitting."
       exit
     end
 
     url = fullURL(id)
     # no valid url (wid doesn't point to guid)
-    #return 'subscriber-only' if url.to_s.empty?
-    # force it even if there's nothing to tell
-    return nil if url.to_s.empty?
+    return [nil, nil] if url.to_s.empty?
     page = ShadowFetch.new(url)
 
     # Tune expiration for young caches:
@@ -192,6 +190,7 @@ class CacheDetails
       page.localExpiry = ttl
     end
     page.fetch()
+    src = page.src
     if page.data
       success = parseCache(page.data)
     else
@@ -207,7 +206,7 @@ class CacheDetails
       end
       debug message
     end
-    return success
+    return [success, src]
   end
 
 # calculate fav factor

lib/shadowget.rb
@@ -74,8 +74,8 @@ class ShadowFetch
   end
 
   def src
-    debug2 "src of last get was #{@@src}"
-    @@src
+    debug2 "src of last get was #{@src}"
+    @src
   end
 
   # returns the cache filename that the URL will be stored as
@@ -164,7 +164,7 @@ class ShadowFetch
 
   # gets the file
   def fetch
-    @@src = nil
+    @src = nil
     time = Time.now
     # this is kind of an ugly hack
     if @localExpiry < 0
@@ -189,8 +189,7 @@ class ShadowFetch
       else
         debug "local cache is only #{age} (<= #{@localExpiry}) sec old, using local file."
         @data = fetchLocal(localfile)
-#        @@src = 'local'
-        @@src = 'l'
+        @src = 'l'	#'local'
         # short-circuit out of here!
         return @data
       end
@@ -212,18 +211,16 @@ class ShadowFetch
       end
     end
     if (@data)
-#      @@src = 'remote'
-      @@src = 'r'
+      @src = 'r'	#'remote'
       size = @data.length
     else
       if (File.readable?(localfile))
         debug "using local cache instead"
         @data = fetchLocal(localfile)
-#        @@src = "local <offline>"
-        @@src = "lo"
+        @src = "lo"	#'local <offline>'
         return @data
       else
-        @@src = nil
+        @src = nil
         debug "ERROR: #{@url} could not be fetched, even by cache"
         return nil
       end
@@ -255,8 +252,7 @@ class ShadowFetch
             # we would lose information by overwriting, but have to concat
             dowrite = false
             @data = olddata + @data
-#            @@src = 'local+remote'
-            @@src = 'l+r'
+            @src = 'l+r'	#'local+remote'
           end #oldPMO
         end #oldcdpf
       end # newPMO

commit bc990e16b651346f0e5d7be2f87038d04a204b51

Fix bug introduced with commit 7714b78, affecting WID/GUID searches.

lib/search.rb
@@ -289,7 +289,7 @@ class SearchCache
   end
 
   def getWidSearchResult(url)
-    data = getPage(url, {})
+    data, src = getPage(url, {})
     if not data
       displayError "No data to be analyzed! Check network connection!"
     end